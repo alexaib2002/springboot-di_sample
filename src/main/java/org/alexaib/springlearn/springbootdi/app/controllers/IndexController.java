f340efe93f49ae2801cd4fb3d438d8bb5edf6852
alexaib2002 <alexaib@outlook.es>
2023-10-31 13:57:47 +0100

    Qualifier for Autowired notation

--- src/main/java/org/alexaib/springlearn/springbootdi/app/controllers/IndexController.java
+++ src/main/java/org/alexaib/springlearn/springbootdi/app/controllers/IndexController.java
@@ -1,8 +1,8 @@
 package org.alexaib.springlearn.springbootdi.app.controllers;
 
 import org.alexaib.springlearn.springbootdi.app.services.IServiceProvider;
-import org.alexaib.springlearn.springbootdi.app.services.SampleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +14,7 @@ public class IndexController {
 
     // Searches an object of the same type inside the Spring container
     @Autowired
+    @Qualifier("failOperation")
     public void setService(IServiceProvider service) {
         this.service = service;
     }
