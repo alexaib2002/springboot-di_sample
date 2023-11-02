597a944df0dcc88e2615d0c5b91daac3f424b409
alexaib2002 <alexaib@outlook.es>
2023-11-02 08:27:45 +0100

    Spring Scopes, migrated Record to Std Class

--- src/main/java/org/alexaib/springlearn/springbootdi/app/domain/Bill.java
+++ src/main/java/org/alexaib/springlearn/springbootdi/app/domain/Bill.java
@@ -6,28 +6,86 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.List;
+import java.util.Objects;
 
 @Component
-public record Bill(
-        @Value("bill.description")
-        String description,
-        @Autowired
-        Customer customer,
-        @Autowired
-        @Qualifier("itemOfficeBill")
-        List<ItemBill> items
-) {
+@RequestScope
+public class Bill implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 0L;
+    @Value("${bill.description}")
+    private final String description;
+    @Autowired
+    private final Customer customer;
+    @Autowired
+    @Qualifier("itemOfficeBill")
+    private final List<ItemBill> items;
 
-        @PostConstruct
-        public void init() {
-                System.out.printf("Customer: %s", customer.name());
-        }
+    public Bill(
+            @Value("${bill.description}")
+            String description,
+            @Autowired
+            Customer customer,
+            @Autowired
+            @Qualifier("itemOfficeBill")
+            List<ItemBill> items
+    ) {
+        this.description = description;
+        this.customer = customer;
+        this.items = items;
+    }
 
-        @PreDestroy
-        public void destroy() {
-                System.err.printf("Destroying %s", this);
-        }
+    @PostConstruct
+    public void init() {
+        System.out.printf("Customer: %s", customer.name());
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.err.printf("Destroying %s", this);
+    }
+
+    @Value("${bill.description}")
+    public String description() {
+        return description;
+    }
+
+    public Customer customer() {
+        return customer;
+    }
+
+    @Qualifier("itemOfficeBill")
+    public List<ItemBill> items() {
+        return items;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Bill) obj;
+        return Objects.equals(this.description, that.description) &&
+                Objects.equals(this.customer, that.customer) &&
+                Objects.equals(this.items, that.items);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(description, customer, items);
+    }
+
+    @Override
+    public String toString() {
+        return "Bill[" +
+                "description=" + description + ", " +
+                "customer=" + customer + ", " +
+                "items=" + items + ']';
+    }
+
 
 }
--- src/main/java/org/alexaib/springlearn/springbootdi/app/domain/Customer.java
+++ src/main/java/org/alexaib/springlearn/springbootdi/app/domain/Customer.java
@@ -2,12 +2,57 @@ package org.alexaib.springlearn.springbootdi.app.domain;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+import java.util.Objects;
 
 @Component
-public record Customer(
-        @Value("${client.name}")
-        String name,
-        @Value("${client.surname}")
-        String surname
-) {
+@RequestScope
+public class Customer {
+    @Value("${client.name}")
+    private final String name;
+    @Value("${client.surname}")
+    private final String surname;
+
+    public Customer(
+            @Value("${client.name}")
+            String name,
+            @Value("${client.surname}")
+            String surname
+    ) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    @Value("${client.name}")
+    public String name() {
+        return name;
+    }
+
+    @Value("${client.surname}")
+    public String surname() {
+        return surname;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Customer) obj;
+        return Objects.equals(this.name, that.name) &&
+                Objects.equals(this.surname, that.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer[" +
+                "name=" + name + ", " +
+                "surname=" + surname + ']';
+    }
+
 }
